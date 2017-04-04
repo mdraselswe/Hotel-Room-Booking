78c19d0f3b01024a0b0553cc98385568469405de
Mohammad Mohibbulllah <mbmohib@gmail.com>
2017-04-04 23:49:36 +0600

    Fix home link

--- view/components/navservice.php
+++ view/components/navservice.php
@@ -1,7 +1,7 @@
 <div class="navigationsection templete clear">
 
     <ul>
-        <li><a  href="/view/home.php">Home</a></li>
+        <li><a  href="/">Home</a></li>
         <li><a id ="active" href="/view/services.php">Services</a></li>
         <li><a href="#">About</a></li>
     </ul>
